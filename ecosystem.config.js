292a1bd2fde6fd2f7dd71a69c962b4745ce49e84
unknown <basseyeyo991@gmail.com>
2026-03-19 16:38:53 +0100

    fix: upload files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,8 @@ module.exports = {
       script: 'node_modules/.bin/next',
       args: 'start',
       cwd: '/home/ec2-user/task-app',
-      instances: 2,
-      exec_mode: 'cluster',
-      wait_ready: true,
-      listen_timeout: 10000,
+      instances: 1,
+      exec_mode: 'fork',
       kill_timeout: 5000,
       env: {
         NODE_ENV: 'production',
